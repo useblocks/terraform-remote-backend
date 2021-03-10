e56ff078725cd3d356aa24182819846006a755e9
Jui-Wen Chen <jui-wen.chen@useblocks.com>
2021-03-10 06:58:13 +0100

    added output vars

--- main.tf
+++ main.tf
@@ -34,6 +34,15 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
 }
 
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
+
 ############## Terraform backend configuration ################
 
 terraform {
